7c9d4d07e4329cdd007c0528cfb780c5efd7722c
kaderbava <ksbava7325@gmail.com>
2020-05-20 18:47:46 +0000

    Revert "cpuidle: lpm-levels: Remove debug event logging"
    This reverts commit f37305e6cd758df73c2864312b1bc83612ac77c8.

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -39,6 +39,7 @@
 #include <soc/qcom/event_timer.h>
 #include <soc/qcom/lpm_levels.h>
 #include <soc/qcom/lpm-stats.h>
+#include <soc/qcom/minidump.h>
 #include <asm/arch_timer.h>
 #include <asm/suspend.h>
 #include <asm/cpuidle.h>
